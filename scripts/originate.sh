e3909f8507f6193606fe834c566d65a9957bcc2d
Asbjørn Enge <asbjorn@hanafjedle.net>
2023-10-24 08:26:53 +0200

    Origination script

--- scripts/originate.sh
+++ scripts/originate.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
+
+PRIVATE_KEY=$(cat secrets/originator.json | jq -r .privkey)
+
+# Create Docker volume
+docker volume create tcp_deploy
+
+# Set permissions for .tezos-client dir
+docker run --rm \
+--user root \
+-v tcp_deploy:/home/tezos/.tezos-client \
+--entrypoint /bin/chmod \
+tezos/tezos:latest -R 777 /home/tezos/.tezos-client
+
+# Import private key
+docker run --rm \
+-v tcp_deploy:/home/tezos/.tezos-client \
+--entrypoint octez-client \
+-it tezos/tezos:latest \
+import secret key deployer unencrypted:$PRIVATE_KEY --force
+
+# Originate
 docker run --rm \
 -v "$(pwd)"/compiled/tcp.tz:/home/tezos/tcp.tz:ro \
+-v tcp_deploy:/home/tezos/.tezos-client \
 --entrypoint octez-client \
 -it tezos/tezos:latest \
 --endpoint $TCP_RPC \
 originate contract scp \
-transferring 0 from tz1UZZnrre9H7KzAufFVm7ubuJh5cCfjGwam \
+transferring 0 from deployer \
 running tcp.tz --burn-cap 0.1
+
+# Remove docker volume
+docker volume rm tcp_deploy
